833e6167245edd4824a0fb07ef02cf859c3b6bf6
Will Forbes <wforbes@ad.boomtownroi.com>
2019-09-25 15:56:16 -0400

    add express and handlebars templating engine

--- app.js
+++ app.js
@@ -1,5 +1,11 @@
 const request = require('request');
 const express = require('express');
+var exphbs = require('express-handlebars'); //handlebars templating
+
+const app = express();
+
+app.engine('handlebars', exphbs());
+app.set('view engine', 'handlebars')
 
 const d = new Date()
 const yesterday = `${d.getFullYear()}-${d.getMonth()}-${d.getDate()-1}`;
@@ -8,7 +14,9 @@ const key = '&api_key=PWpg8az80VaMoqfYAtuIKrlAJv4YhHsBuVodHQFl';
 const frontHazQuery = `?earth_date=${yesterday}&camera=fhaz`;
 const url = 'https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos' + frontHazQuery + key
 
-
+app.get('/', function (req, res) {
+    res.send('Hello World')
+})
 
 request ({url, json: true}, (error, response, body) => {
     if (error) {
@@ -17,4 +25,8 @@ request ({url, json: true}, (error, response, body) => {
         const img = body.photos[0].img_src
         console.log(img)
     }
+})
+
+app.listen(3000, () => {
+    console.log('server started!')
 })
