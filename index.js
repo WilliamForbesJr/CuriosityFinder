2934f692a6aad1d78a2a9737f886efb125a58dad
Will <forbes8963@gmail.com>
2019-09-30 19:33:11 -0400

    add function to load random array index for body response. This will allow user to receive a completely unique image from NASAs JSON payload

--- index.js
+++ index.js
@@ -1,37 +1,40 @@
 // Modules
 const request       = require('request');
 const express       = require('express');
-const hbs           = require('hbs')
-const APIManager    = require('./utils/APIManager.js')
+const hbs           = require('hbs');
+const APIManager    = require('./utils/APIManager.js');
 const app           = express();
 
 //view engine setup for templating
-app.set('view engine', 'hbs')
-app.use(express.static('public')) //static dir
+app.set('view engine', 'hbs');
+app.use(express.static('public')); //static dir
 
 app.get('/', (req, res) => {
 
-    const url = APIManager.buildURL()
+    const url = APIManager.buildURL();
 
     request({url, json: true}, (error, response, body) => {
         if (error) {
-            res.send(error)
+            res.send(error);
         } else {
+
+            const index = APIManager.loadRandomArrayIndex(body.photos); //creating constant for photos array based on length of array received
+
             res.render('index', {
-                date: body.photos[0].earth_date,
-                img: body.photos[0].img_src,
-                sol: body.photos[0].sol,
-                status: body.photos[0].rover.status,
-                cam: body.photos[0].camera.full_name
-            })
+                date: body.photos[index].earth_date,
+                img: body.photos[index].img_src,
+                sol: body.photos[index].sol,
+                status: body.photos[index].rover.status,
+                cam: body.photos[index].camera.full_name
+            });
         }
-    })
-})
+    });
+});
 
 let port = process.env.PORT;
 
 app.listen(port || 3000, () => {
-    console.log('server started!')
+    console.log('server started!');
 })
 
 module.exports = app; //to test
--- utils/APIManager.js
+++ utils/APIManager.js
@@ -1,22 +1,26 @@
 const APIManager = (function () {
     return {
 
-        getDate: function () {
-            const d = new Date()
-            return `?earth_date=${d.getFullYear()}-${d.getMonth() + 1}-${d.getDate() - 2}`
-        },
-
         key: process.env.APIKEY, //key stored as local var
 
         cams: ['fhaz', 'rhaz', 'chemcam', 'mast', 'mahli', 'mardi', 'navcam', 'pancam'],
         //need to check refresh rates for all cams other than fhaz and rhaz as they are not daily use idx 0 and 1 for now
 
+        getDate: function () {
+            const d = new Date();
+            return `?earth_date=${d.getFullYear()}-${d.getMonth() + 1}-${d.getDate() - 2}`;
+        },
+
         loadRandomCam: function () {
-            return Math.floor(Math.random() * 3);
+            return Math.floor(Math.random() * 2);
         }, //keeping range between 0-2 for now as API calls are still not consistent with all of NASA's cams.
 
         buildURL: function () {
-            return 'https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos' + this.getDate() + `&camera=${this.cams[this.loadRandomCam()]}` + this.key
+            return 'https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos' + this.getDate() + `&camera=${this.cams[this.loadRandomCam()]}` + this.key;
+        },
+
+        loadRandomArrayIndex: function(body) {
+            return Math.floor(Math.random() * body.length);
         },
     }
 }());
