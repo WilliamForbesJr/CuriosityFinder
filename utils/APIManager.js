9cba5f118ecffa152237136205eefd8a4994162b
Will Forbes <wforbes@ad.boomtownroi.com>
2019-09-30 10:42:11 -0400

    revert to original API manager module as heroku is bombing out after refactor

--- utils/APIManager.js
+++ utils/APIManager.js
@@ -1,9 +1,4 @@
 const APIManager = (function () {
-
-    const key = process.env.APIKEY; //key stored as local var
-    const cams = ['fhaz', 'rhaz', 'chemcam', 'mast', 'mahli', 'mardi', 'navcam', 'pancam',];
-        //need to check refresh rates for all cams other than fhaz and rhaz as they are not daily use idx 0 and 1 for now
-
     return {
 
         getDate: function () {
@@ -11,14 +6,21 @@ const APIManager = (function () {
             return `?earth_date=${d.getFullYear()}-${d.getMonth() + 1}-${d.getDate() - 2}`
         },
 
+        key: process.env.APIKEY, //key stored as local var
+
+        cams: ['fhaz', 'rhaz', 'chemcam', 'mast', 'mahli', 'mardi', 'navcam', 'pancam'],
+        //need to check refresh rates for all cams other than fhaz and rhaz as they are not daily use idx 0 and 1 for now
+
         loadRandomCam: function () {
             return Math.floor(Math.random() * 3);
         }, //keeping range between 0-2 for now as API calls are still not consistent with all of NASA's cams.
 
         buildURL: function () {
-            return `https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos${this.getDate()}&camera=${cams[this.loadRandomCam()]}${key}`
+            return 'https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos' + this.getDate() + `&camera=${this.cams[this.loadRandomCam()]}` + this.key
         },
     }
 }());
+
+module.exports = APIManager;
 
 module.exports = APIManager;
